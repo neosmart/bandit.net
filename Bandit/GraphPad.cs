0e3d094de3dd28afd4211bd90366a8e4b106e08f
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2016-12-09 15:18:47 -0600

    Safer handling of zero values in GraphPad functions

--- Bandit/GraphPad.cs
+++ Bandit/GraphPad.cs
@@ -24,6 +24,11 @@ namespace NeoSmart.Bandit
     {
         static public decimal ChiSquare(BinaryResult variant1, BinaryResult variant2)
         {
+            if (variant1.Outcome1 == 0 || variant2.Outcome1 == 0 || variant1.Outcome2 == 0 || variant2.Outcome2 == 0)
+            {
+                return 0;
+            }
+
             var request = new WebClient();
             var parameters = new NameValueCollection
             {
@@ -40,10 +45,19 @@ namespace NeoSmart.Bandit
                 {"D", variant2.Outcome2.ToString()}
             };
 
-            var result = Encoding.UTF8.GetString(request.UploadValues("http://graphpad.com/quickcalcs/contingency2/", "POST", parameters));
-            result = Regex.Replace(result, @"\s+", " ", RegexOptions.Multiline);
-            var rMatch = Regex.Match(result, "The two-tailed P value equals (\\d+\\.\\d+)", RegexOptions.Multiline);
-            return decimal.Parse(rMatch.Groups[1].Captures[0].Value.ToString());
+            try
+            {
+                var result =
+                    Encoding.UTF8.GetString(request.UploadValues("http://graphpad.com/quickcalcs/contingency2/", "POST",
+                        parameters));
+                result = Regex.Replace(result, @"\s+", " ", RegexOptions.Multiline);
+                var rMatch = Regex.Match(result, "The two-tailed P value equals (\\d+\\.\\d+)", RegexOptions.Multiline);
+                return decimal.Parse(rMatch.Groups[1].Captures[0].Value.ToString());
+            }
+            catch
+            {
+                return -1;
+            }
         }
     }
 }
